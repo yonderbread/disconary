e46ab9c6f8a82d946fbfbdaaed00fc3c27a7344a
yonderbread <realmctbf@gmail.com>
2020-09-14 21:32:27 -0700

    :zap: Pagination!

--- commands.py
+++ commands.py
@@ -1,26 +1,39 @@
 from discord.ext import commands
+from disputils import BotEmbedPaginator
+from discord import Embed
 from dictionary import Dictionary
-import discord
 
 dictionary = Dictionary()
+
 
 class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def dict(self, ctx, term: str):
         message = ctx.message
         res = dictionary.get_definitions_for(term)
-        if not 'message' in res:
-            word = res[0]['word']
-            meaning = res[0]['meanings'][0]['definitions'][0]['definition']
-            part = res[0]['meanings'][0]['partOfSpeech']
-            await message.channel.send(
-                embed=discord.Embed(
-                    title=word,
-                    description=meaning
+        if not "message" in res:
+            data = res[0]
+            meaning = res[0]["meanings"][0]["definitions"][0]["definition"]
+            part = res[0]["meanings"][0]["partOfSpeech"]
+
+            meanings = []
+            count = len(data["meanings"])
+            for meaning in data["meanings"]:
+                pos = meaning["partOfSpeech"]
+                definitions = ""
+                for defi in meaning["definitions"]:
+                    definitions += ' - ' + defi["definition"] + '\n\n'
+                meanings.append(
+                    Embed(title=data['word'],
+                    description=f'```{definitions}```',
+                    color=0x115599)
                 )
-            )
+            
+            paginator = BotEmbedPaginator(ctx, meanings)
+            await paginator.run()
+
         else:
-            await message.channel.send(f'Could not find definition for term `{term}`.')
+            await message.channel.send(f"Could not find definition for term `{term}`.")
